c3cc5d7854dc1608ebad82e5ae730e71d935d5ba
David Honeycutt <139165770+thefrenchman1@users.noreply.github.com>
2024-01-14 20:32:22 -0500

    Update webpack.config.js
    removed extra path

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 const path = require('path');
 
